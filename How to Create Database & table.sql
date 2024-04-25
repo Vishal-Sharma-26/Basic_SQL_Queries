a5d6f1486aaeb5687d876dfa053ae96ca069b53e
Vishal Sharma <vishal.en.work.@gmail.com>
2024-04-25 15:06:59 +0530

    Initial Commit

---
+++ How to Create Database & table.sql
@@ -0,0 +1,8 @@
+create database Employees
+
+Create Table EmployeeData
+(Id int,
+FirstName varchar(20),
+LastName varchar(20),
+Department varchar(30),
+Salary int)
---
+++ How to insert value.sql
@@ -0,0 +1,23 @@
+Insert into EmployeeData VALUES
+(1001, 'Rohan', 'Das', 'Sales', 30000),
+(1002, 'Sunita', 'Goyal', 'Marketing', 33000),
+(1003, 'Anjali', 'Arora', 'Development', 60000),
+(1004, 'Shubham', 'Kumar', 'HR', 35000),
+(1005, 'Tarun', 'Kumar', 'Testing', 40000),
+(1006, 'Vishal', 'Sharma', 'Analyst', 80000),
+(1007, 'Mohit', 'Sharma', 'Analyst', 73000),
+(1008, 'Ram', 'Upadhyay', 'Marketing', 30000),
+(1009, 'Ayushi', 'Bansal', 'Sales', 42000),
+(1010, 'Chinky', 'Goyal', 'Accountent', 22000),
+(1011, 'Nakul', 'Varma', 'Sales', 30000),
+(1012, 'Gulhan', 'Saraswat', 'HR', 29000),
+(1013, 'Gaurav', 'Chaudhary', 'Sales', 30900),
+(1014, 'Khushi', 'Sharma', 'Development', 56000),
+(1015, 'Himanshu', 'Agrawal', 'Sales', 38000),
+(1016, 'Varsha', 'Parashar', 'Accountent', 30000),
+(1017, 'Nandini', 'Sharma', 'HR', 30000),
+(1018, 'Sohan', 'Das', 'Testing', 35000),
+(1019, 'Varun', 'Upadhyay', 'HR', 40000),
+(1020, 'Nandini', 'Upadhyay', 'Testing', 35000)
+
+select * from EmployeeData
